86a762783f2d34d3278e4ff131359f2e97f2444d
Andriy <andygrigs88@gmail.com>
2023-11-12 15:03:18 +0200

    registartion interface

--- client/src/app/features/AuthByUserName/ui/LoginForm/LoginForm.tsx
+++ client/src/app/features/AuthByUserName/ui/LoginForm/LoginForm.tsx
@@ -3,6 +3,7 @@ import cls from "./LoginForm.module.scss"
 import { classNames } from "@/shared/lib/classNames"
 import { Button } from "@/shared/ui/Button/Button";
 import { Input } from "@/shared/ui/Input/ui/Input";
+import { AppLink } from "@/shared/ui/AppLink/AppLink";
 
 export interface LoginFormProps {
     className?: string;
@@ -11,13 +12,13 @@ export interface LoginFormProps {
 export const LoginForm = ({ className }: LoginFormProps) => {
     return (
         <div className={classNames(cls.LoginForm, {}, [className])}>
-            <Input
-                placeholder="Login"
-                className={cls.input} />
-            <Input
-                placeholder="Password"
-                className={cls.input} />
-            <Button className={cls.loginBtn} >Enter</Button>
+            <h2 className={cls.title}>Login</h2>
+            <Input placeholder="Login" className={cls.input} />
+            <Input placeholder="Password" className={cls.input} />
+            <Button className={cls.loginBtn}>Enter</Button>
+            <p className={cls.registrationLink}>
+                Don't have an account? <AppLink to="/registration">Register</AppLink>
+            </p>
         </div>
     )
 };
