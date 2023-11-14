0fa60ed0f5e474922cfbccce1402abdb18d2ad04
Andriy <andygrigs88@gmail.com>
2023-11-14 14:57:20 +0200

    working

--- client/firebase.ts
+++ client/firebase.ts
@@ -1,11 +1,21 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// const firebaseConfig = {
-//     apiKey: import.meta.env.FIREBASE_API_KEY,
-//     authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
-//     projectId: import.meta.env.FIREBASE_PROJECT_ID,
-//     storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
-//     messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
-//     appId: import.meta.env.FIREBASE_ID
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    // apiKey: import.meta,
+    // authDomain: ,
+    // projectId:,
+    // storageBucket: ,
+    // messagingSenderId:,
+    // appId: ,
+    // measurementId: 
+};
 
-// const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -4,7 +4,6 @@
 
 interface ImportMetaEnv {
     readonly VITE_APP_TITLE: string
-    // more env variables...
 }
 
 interface ImportMeta {
