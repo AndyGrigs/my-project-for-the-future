89d76a624826e7992f3cb28a13fbaacf7eb17677
Andriy <andygrigs88@gmail.com>
2023-12-14 08:09:00 +0200

    fixing bags

--- client/src/app/entities/User/model/slice/userSlice.ts
+++ client/src/app/entities/User/model/slice/userSlice.ts
@@ -1,15 +1,20 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { UserSchema } from "../types/user";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { User, UserSchema } from "../types/user";
 
 const initialState: UserSchema = {
-
+    authData: undefined
 };
 
 export const userSlice = createSlice({
     name: "user",
     initialState,
     reducers: {
-
+        setAuthData(state, action: PayloadAction<User>) {
+            state.authData = action.payload;
+        },
+        clearAuthData(state) {
+            state.authData = undefined;
+        },
     },
 });
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,9 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app/App.js";
 import { BrowserRouter } from "react-router-dom";
 import ThemeProvider from "./app/providers/ThemeProvider.js";
 import "./app/styles/index.scss";
 import { StoreProvider } from "./app/providers/StoreProvider/index.js";
-import "../firebase.ts"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <StoreProvider>
