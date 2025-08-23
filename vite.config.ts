7ab705df06078434fdff45332de97e2b81dcf3ca
Andriy <andygrigs88@gmail.com>
2025-08-23 09:35:30 +0200

    fix config errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import svgr from 'vite-plugin-svgr';
-import dotenv from 'dotenv';
-
-// Load environment variables from .env
-dotenv.config();
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr({
-    // svgr options: https://react-svgr.com/docs/options/
-    svgrOptions: {
-      // ...
-    },
-
-    // esbuild options, to transform jsx to js
-    esbuildOptions: {
-      // ...
-    },
-
-    // A minimatch pattern, or array of patterns, which specifies the files in the build the plugin should include.
-    include: "**/*.svg?react",
-
-    //  A minimatch pattern, or array of patterns, which specifies the files in the build the plugin should ignore. By default no files are ignored.
-    exclude: "",
-  })],
-  resolve: {
-    alias: [{ find: "@", replacement: "/src" }],
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
   },
 });
